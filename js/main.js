f584f91ef3ed2d56197cdfaefc033510281bbe6f
Jason-Donmoyer <j.donmoyer523@gmail.com>
2019-12-08 07:12:01 -0500

    fixed email validation bug

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
-function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
 
+// Get submit button
 let submitButton = document.getElementById('submit-button');
 
+// Event listener for submit button
 submitButton.addEventListener('click', (e) => {
 
-    let inputFields = document.getElementsByTagName('input');
-    let inputArr = [];
+    // Selects inputs that have not-email class
+    let inputArr = document.querySelectorAll('[class=not-email]');
+    // Selects email input -- seperate for validation purposes
+    let emailInput = document.getElementById('email');
+    // Array to hold inputs that are valid -- also used to check if all inputs are valid
     let dataArr = [];
-    for (let i = 0; i < inputFields.length; i++) {
-        if (inputFields[i].hasAttribute('placeholder')) {
-            inputArr.push(inputFields[i]);
-        }
+
+    
+    if ((emailInput.value) === '' || (validateEmail(emailInput.value) === false)) {
+        e.preventDefault();
+        invalidItem(emailInput);
+        // emailInput.nextElementSibling.style.display = 'block';
+        // emailInput.style.backgroundImage = 'url(images/icon-error.svg)';
+        // emailInput.style.borderColor = 'var(--color-red)';
+        emailInput.style.color = 'var(--color-red)';
+    } else {
+        emailInput.nextElementSibling.style.display = 'none';
+        emailInput.style.backgroundImage = 'none';
+        emailInput.style.borderColor = 'var(--color-grayish-blue)';
+        emailInput.style.color = '#000';
+        dataArr.push(emailInput);
     }
 
+    
+    // Loop through inputs to validate
     for (let i = 0; i < inputArr.length; i++) {
         
         if (inputArr[i].value === '') {
@@ -23,30 +37,29 @@ submitButton.addEventListener('click', (e) => {
             inputArr[i].nextElementSibling.style.display = 'block';
             inputArr[i].style.backgroundImage = 'url(images/icon-error.svg)';
             inputArr[i].style.borderColor = 'var(--color-red)';
-        } else if ((inputArr[i] !== '') && (inputArr[i].hasAttribute('type', 'email'))) {
-            if (validateEmail(inputArr[i].value) === false) {
-                console.log('FALSE');
-                e.preventDefault();
-                inputArr[i].nextElementSibling.style.display = 'block';
-                inputArr[i].style.backgroundImage = 'url(images/icon-error.svg)';
-                inputArr[i].style.borderColor = 'var(--color-red)';
-                inputArr[i].style.color = 'var(--color-red)';
-            } else {
-                inputArr[i].nextElementSibling.style.display = 'none';
-                inputArr[i].style.backgroundImage = 'none';
-                inputArr[i].style.borderColor = 'var(--color-grayish-blue)';
-                dataArr.push(inputArr[i]);
-            }
         } else if (inputArr[i].value !== '') {
             inputArr[i].nextElementSibling.style.display = 'none';
             inputArr[i].style.backgroundImage = 'none';
             inputArr[i].style.borderColor = 'var(--color-grayish-blue)';
             dataArr.push(inputArr[i]);
-        }
+        }    
     }
 
     console.log(dataArr);
+    if (dataArr.length === 4) {
+        alert('Thank You! Your information has been submitted.');
+    }
 });
 
+function invalidItem(input) {
+    input.nextElementSibling.style.display = 'block';
+    input.style.backgroundImage = 'url(images/icon-error.svg)';
+    input.style.borderColor = 'var(--color-red)';
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
 
