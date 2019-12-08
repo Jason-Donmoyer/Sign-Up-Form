58bd201e451754811c5cb5ed73d49073ce17f06d
Jason-Donmoyer <j.donmoyer523@gmail.com>
2019-12-08 05:47:31 -0500

    js email validation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
 let submitButton = document.getElementById('submit-button');
 
@@ -14,12 +17,29 @@ submitButton.addEventListener('click', (e) => {
     }
 
     for (let i = 0; i < inputArr.length; i++) {
+        
         if (inputArr[i].value === '') {
             e.preventDefault();
             inputArr[i].nextElementSibling.style.display = 'block';
+            inputArr[i].style.backgroundImage = 'url(images/icon-error.svg)';
             inputArr[i].style.borderColor = 'var(--color-red)';
+        } else if ((inputArr[i] !== '') && (inputArr[i].hasAttribute('type', 'email'))) {
+            if (validateEmail(inputArr[i].value) === false) {
+                console.log('FALSE');
+                e.preventDefault();
+                inputArr[i].nextElementSibling.style.display = 'block';
+                inputArr[i].style.backgroundImage = 'url(images/icon-error.svg)';
+                inputArr[i].style.borderColor = 'var(--color-red)';
+                inputArr[i].style.color = 'var(--color-red)';
+            } else {
+                inputArr[i].nextElementSibling.style.display = 'none';
+                inputArr[i].style.backgroundImage = 'none';
+                inputArr[i].style.borderColor = 'var(--color-grayish-blue)';
+                dataArr.push(inputArr[i]);
+            }
         } else if (inputArr[i].value !== '') {
             inputArr[i].nextElementSibling.style.display = 'none';
+            inputArr[i].style.backgroundImage = 'none';
             inputArr[i].style.borderColor = 'var(--color-grayish-blue)';
             dataArr.push(inputArr[i]);
         }
@@ -27,4 +47,6 @@ submitButton.addEventListener('click', (e) => {
 
     console.log(dataArr);
 });
+
+
 
